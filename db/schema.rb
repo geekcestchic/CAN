1684ed080b7274ce7934a545d7a25e78bd6068d3
Chris Ijoyah <chris@ijoyah.com>
2015-03-02 17:38:16 +0000

    add follower count, follower names to user show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302142827) do
+ActiveRecord::Schema.define(version: 20150302152633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,8 @@ ActiveRecord::Schema.define(version: 20150302142827) do
     t.string   "avatar"
     t.text     "bio"
     t.string   "location"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
