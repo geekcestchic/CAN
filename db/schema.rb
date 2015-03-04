efd513da9c7ceb8fe47b5da3482df6ce318cf680
Alex Howard <alexhoward007@hotmail.com>
2015-03-04 18:33:18 +0000

    made some changes to users, still need to fix the project upload per user

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150304133532) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,16 +50,6 @@ ActiveRecord::Schema.define(version: 20150304133532) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
-
   create_table "projects", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
@@ -92,8 +80,8 @@ ActiveRecord::Schema.define(version: 20150304133532) do
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "similarities", force: :cascade do |t|
-    t.integer "similee_id"
     t.integer "similar_id"
+    t.integer "similee_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -118,11 +106,9 @@ ActiveRecord::Schema.define(version: 20150304133532) do
     t.string   "provider"
     t.string   "uid"
     t.string   "role"
-
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "identities", "users"
 end
