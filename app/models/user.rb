bbfe85e6c7d5816405ba4089df9ab48c2e70c032
Chris Ijoyah <chris@ijoyah.com>
2015-03-04 13:07:19 +0000

    added omniauth for facebook and twitter, working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:twitter, :facebook]
 
   has_many :projects
   has_many :comments
@@ -15,6 +16,17 @@ class User < ActiveRecord::Base
 
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.firstname = auth.info.firstname
+      user.avatar = auth.info.avatar
+    end
+  end
 
   
   #Follows a user.
