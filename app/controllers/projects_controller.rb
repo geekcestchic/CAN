b3bb1e0cab3e4c0f8aa6b700346c70c613fbf9c1
Alex Howard <alexhoward007@hotmail.com>
2015-03-05 10:21:00 +0000

    added styling to categories

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -28,6 +28,10 @@ class ProjectsController < ApplicationController
     # render json: @project, status: :created
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
   def update
     @project = Project.find(params[:id])
     @project.update project_params
