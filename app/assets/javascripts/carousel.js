e2173c0277dcaee5075a5e033c093406c89962bf
Alex Howard <alexhoward007@hotmail.com>
2015-03-05 18:17:08 +0000

    improved welcome page

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -7,18 +7,37 @@ function request(method, url, data){
   });
 }
 
+// function getSlideShow(){
+//   //create an array of images from the 'images' folder
+//   var images = []
+//   request('GET','/projects', null).done(function(projects){
+//     $.each(projects, function(index,project){
+//       images.push(project.image.url);
+//     });
+//     images = images.filter(Boolean)
+//     addToNav(images);
+//   });
+// }
+
 function getSlideShow(){
   //create an array of images from the 'images' folder
   var images = []
+  var titles = []
+  var ids = []
   request('GET','/projects', null).done(function(projects){
     $.each(projects, function(index,project){
       images.push(project.image.url);
+      titles.push(project.title);
+      ids.push(project.id);
     });
-    addToNav(images);
+    images = images.filter(Boolean)
+    addToNav(images, titles, ids);
   });
 }
 
-function addToNav(images){
+
+
+function addToNav(images,titles,ids){
   $('<div class="slideshow-nav"></div>').insertAfter('.slideshow-container');
 
  //loop through the images and add to the empty container on the page
@@ -26,6 +45,16 @@ function addToNav(images){
     $('<img src="' + value + '">').appendTo('.slideshow-images');
     $('<a href="#"></a>').appendTo('.slideshow-nav');
   });
+  $.each(titles, function(index, value){
+    console.log(value)
+    $('<li>'+value+'</li>').appendTo('.project-links');
+  });
+  // $.each(ids, function(index, value){
+  //   $('<img src="' + value + '">').appendTo('.slideshow-images');
+  //   $('<a href="#"></a>').appendTo('.slideshow-nav');
+  // });
+
+  
 
   //fade the images in
   $('.slideshow-images img').animate({opacity: 1}, 3000);
@@ -34,24 +63,27 @@ function addToNav(images){
   var imageWidth = $('.slideshow-container').width();
   $('.slideshow-nav a').on('click', function(){
     var index = $(this).index();
-    $('.slideshow-images').animate({
-      'margin-left': -index * imageWidth
-    }, 750);
-    $("currentSlide").wrap("<a href='/projects/new'></a>");
+    $('.slideshow-images').animate({'margin-left': -index * imageWidth}, 750);
+    $(this).addClass('is-active').siblings().removeClass('is-active');
+  });
+
+  $('.project-links li').on('click', function(){
+    var index = $(this).index();
+    $("currentLink").wrap("<a href='/projects/new'></a>");
     $(this).addClass('is-active').siblings().removeClass('is-active');
   });
 
   //auto advance the slideshow
   var currentSlide = 0;
+  var currentLink = 0
   setInterval(function(){
-    //trigger a click on the first nav item
-    //trigger a click on the next nav item
-    //trigger a click on the last nav item
-    //trigger a click on the first nav item
     if (currentSlide >= images.length){
       currentSlide = 0;
+      currentLink = 0;
     }
     $('.slideshow-nav a').eq(currentSlide++).trigger('click')
+    // $('.project-links li').eq(currentLink++).toggleClass('is-active')
+    $('.project-links li').eq(currentLink++).trigger('click')
   }, 3000)
 }
 
